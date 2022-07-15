7a6fc455be665af331993965a0e9270e7c168c9c
vijay04 <vijay.prasad@dojima.network>
2022-07-15 08:59:42 +0530

    corrected url

--- get_url.sh
+++ get_url.sh
@@ -1,4 +1,4 @@
 INGRESS_HOST=${INGRESS_HOST:="$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')"}
-INGRESS_PORT=${INGRESS_PORT:="export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')"}
+INGRESS_PORT=${INGRESS_PORT:="$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')"}
 APP_NAME=${APP_NAME:="dojima-wallet"}
 echo “http://$INGRESS_HOST:$INGRESS_PORT/${APP_NAME}”
