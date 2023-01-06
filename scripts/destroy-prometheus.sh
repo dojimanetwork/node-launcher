c1008689495dd92a5f656c29fc088a3746adb285
bhaagiKenpachi <bhagath.reddy@dojima.network>
2023-01-06 22:43:59 +0530

    dev: mod: renamed prometheus system -> prometheus

--- scripts/destroy-prometheus.sh
+++ scripts/destroy-prometheus.sh
@@ -4,12 +4,12 @@ set -e
 
 source ./scripts/core.sh
 
-helm get all prometheus -n prometheus-system
+helm get all prometheus -n prometheus
 echo -n "The above resources will be deleted "
 confirm
 
 echo "=> Deleting Prometheus/Grafana Stack"
-helm delete prometheus -n prometheus-system
+helm delete prometheus -n prometheus
 kubectl delete crd alertmanagerconfigs.monitoring.coreos.com
 kubectl delete crd alertmanagers.monitoring.coreos.com
 kubectl delete crd podmonitors.monitoring.coreos.com
@@ -18,5 +18,5 @@ kubectl delete crd prometheuses.monitoring.coreos.com
 kubectl delete crd prometheusrules.monitoring.coreos.com
 kubectl delete crd servicemonitors.monitoring.coreos.com
 kubectl delete crd thanosrulers.monitoring.coreos.com
-kubectl delete namespace prometheus-system
+kubectl delete namespace prometheus
 echo
