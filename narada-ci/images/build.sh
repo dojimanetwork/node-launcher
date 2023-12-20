4481b539910c5f66baa6527e06034168423a685f
bhaagiKenpachi <bhagath.reddy@dojima.network>
2023-12-20 18:53:46 +0530

    stagenet: build narada ci changes

--- narada-ci/images/build.sh
+++ narada-ci/images/build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-REGISTRY="asia.gcr.io/prod-dojima/narada-ci"
+REGISTRY="asia-south1-docker.pkg.dev/prod-dojima/stagenet/narada-ci"
 
 #docker login -u "${CI_REGISTRY_USER}" -p "${CI_REGISTRY_PASSWORD}" "${CI_REGISTRY}"
 
@@ -10,14 +10,14 @@ find narada-ci/images/ -name version -printf '%h\n' | xargs basename -a | while 
   version=$(cat "narada-ci/images/$image/version")
 
   # check to see if image version is already published
-#  if docker manifest inspect "$REGISTRY:${image}-${version}" > /dev/null 2>&1; then
-#    echo "Image ${image}:${version} already published."
-#  else
+  if docker manifest inspect "$REGISTRY:${image}-${version}" > /dev/null 2>&1; then
+    echo "Image ${image}:${version} already published."
+  else
     echo "Building image $image:$version..."
     docker build -t "$REGISTRY:$image-$version" "narada-ci/images/$image"
     if [ "$CI_COMMIT_BRANCH" = "gcp_stagenet" ]; then
       docker push "$REGISTRY:$image-$version"
     fi
-#  fi
+  fi
 
 done
