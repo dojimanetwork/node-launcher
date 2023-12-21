1c64e6b4cdf9bc0b44bf946a5b52b2a1dbe6bb0b
bhaagiKenpachi <bhagath.reddy@dojima.network>
2023-12-21 12:09:26 +0530

    stagenet: binance seeds revert

--- narada-ci/images/binance-daemon/entrypoint.sh
+++ narada-ci/images/binance-daemon/entrypoint.sh
@@ -26,7 +26,7 @@ sed -i "s/state:info/state:error/g" "${BNCHOME}/config/config.toml"
 
 # fix testnet seed
 if [ "${BNET}" == "testnet" ]; then
-  sed -i -e "s/seeds =.*/seeds = \"2726550182cbc5f4618c27e49c730752a96901e8@184.72.122.37:27146,9612b570bffebecca4776cb4512d08e252119005@3.114.127.147:27146\"/g" "${BNCHOME}/config/config.toml"
+  sed -i -e "s/seeds =.*/seeds = \"3a18f4189fe54af14e9c2da1d6edeb9a23eca445@184.72.122.37:27146,52157533d7ae8089517f4f7e1de9282276e8047c@3.114.127.147:27146\"/g" "${BNCHOME}/config/config.toml"
 fi
 
 echo "Running $0 in $PWD"
