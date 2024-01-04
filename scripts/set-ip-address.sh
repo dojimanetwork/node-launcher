056be73270d6e97f3c02bfcaf13c1f8730d94dff
bhaagiKenpachi <bhagath.reddy@dojima.network>
2024-01-04 16:35:40 +0530

    devnet: validator changes - 1

--- scripts/set-ip-address.sh
+++ scripts/set-ip-address.sh
@@ -6,7 +6,7 @@ source ./scripts/core.sh
 
 get_node_info_short
 
-echo "=> Setting THORNode IP address"
-kubectl exec -it -n "$NAME" deploy/hermesnode -- /kube-scripts/set-ip-address.sh "$(kubectl -n "$NAME" get configmap gateway-external-ip -o jsonpath="{.data.externalIP}")"
+echo "=> Setting Hermesnode IP address"
+kubectl exec -it -n "$NAME" deploy/hermesnode -- /kube-scripts/set-ip-address.sh "$(kubectl -n "$NAME" get configmap hermesnode-external-ip -o jsonpath="{.data.externalIP}")"
 sleep 5
-echo THORNode IP address updated
+echo Hermesnode IP address updated
--- scripts/set-node-keys.sh
+++ scripts/set-node-keys.sh
@@ -6,7 +6,7 @@ source ./scripts/core.sh
 
 get_node_info_short
 
-echo "=> Setting THORNode keys"
+echo "=> Setting Hermesnode keys"
 kubectl exec -it -n "$NAME" -c hermesnode deploy/hermesnode -- /kube-scripts/set-node-keys.sh
 sleep 5
-echo THORNode Keys updated
+echo Hermesnode Keys updated
