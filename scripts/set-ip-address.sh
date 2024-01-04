88069dd3d6c7dbddc5db72aa3f2d9157a01a0f7f
bhaagiKenpachi <bhagath.reddy@dojima.network>
2024-01-04 20:45:22 +0530

    devnet: fixed retry script and validator make file changes

--- scripts/set-ip-address.sh
+++ scripts/set-ip-address.sh
@@ -6,7 +6,10 @@ source ./scripts/core.sh
 
 get_node_info_short
 
+GATEWAY=$(kubectl -n "${NAME}" get configmap hermesnode-external-ip -o jsonpath="{.data.externalIP}")
+IP_ADDRESS="${IP_ADDRESS:-${GATEWAY}}"
+
 echo "=> Setting Hermesnode IP address"
-kubectl exec -it -n "$NAME" deploy/hermesnode -- /kube-scripts/set-ip-address.sh "$(kubectl -n "$NAME" get configmap hermesnode-external-ip -o jsonpath="{.data.externalIP}")"
+kubectl exec -it -n "$NAME" deploy/hermesnode -- /kube-scripts/set-ip-address.sh "$IP_ADDRESS"
 sleep 5
-echo Hermesnode IP address updated
+echo Hermesnode IP address updated to "${IP_ADDRESS}"
