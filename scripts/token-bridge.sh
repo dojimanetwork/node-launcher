f5967d668737edc0f781160e0baded309feac893
bhaagiKenpachi <bhagath.reddy@dojima.network>
2024-12-18 23:32:04 +0530

    successfull custom token deployment arbitrum

--- scripts/token-bridge.sh
+++ scripts/token-bridge.sh
@@ -5,19 +5,28 @@ set -e
 # create recover pod
 echo "creating arbitrum token bridge contracts pod"
 
-rollupAddr=$(kubectl exec -i -t arbitrum-5db69b8498-f6svk -c arbitrum-stack-sequencer -n arbitrum-rollup-1 -- cat /root/l2_config/deployment.json | jq -r '.rollup')
+rollupAddr=$(kubectl exec -it -c arbitrum-stack-sequencer deploy/arbitrum -n arbitrum-rollup-1 -- cat /root/l2_config/deployment.json | jq -r '.rollup')
+deploymentJson=$(kubectl exec -it -c arbitrum-stack-sequencer deploy/arbitrum -n arbitrum-rollup-1 -- cat /root/l2_config/deployment.json)
 
 export ROLLUP_ADDR=$rollupAddr
-envsubst '${ROLLUP_ADDR} ${DEPLOYER_PRIVKEY_NAME} ${PARENT_RPC} ${BS_EXTERNAL_IP} $SEQUENCER_PRIVKEY_NAME' < dependency_charts/token-bridge/deployment.yaml > dependency_charts/token-bridge/deployment_temp.yaml
+export DEPLOYMENT_JSON=$(echo $deploymentJson | jq -c .)
+envsubst '${ROLLUP_ADDR} ${DEPLOYER_PRIVKEY_NAME} ${PARENT_RPC} ${BS_EXTERNAL_IP} $SEQUENCER_PRIVKEY_NAME $DEPLOYMENT_JSON' < dependency_charts/token-bridge/deployment.yaml > dependency_charts/token-bridge/deployment_temp.yaml
 
 kubectl apply -f dependency_charts/token-bridge/deployment_temp.yaml
 
 # reset node state
 echo "waiting for recover pod to be ready..."
-kubectl wait --for=condition=complete pods/token-bride-contracts-arbitrum -n "${NAME}" --timeout=5m >/dev/null 2>&1
+kubectl wait --for=condition=complete pods/token-bridge-contracts-arbitrum -n "${NAME}" --timeout=5m
 
 echo "=> ${boldgreen}Proceeding to clean up recovery pod and restart hermesnode${reset}"
 confirm
 
-echo "cleaning up recover pod"
-kubectl -n "${NAME}" delete pod/restore-external-hermesnode
+# Check if the wait command was successful
+if [ $? -eq 0 ]; then
+  echo "cleaning up recover pod"
+  kubectl -n "${NAME}" delete pod/token-bridge-contracts-arbitrum
+else
+  echo "Pod $POD_NAME did not complete within the timeout period."
+fi
+
+
